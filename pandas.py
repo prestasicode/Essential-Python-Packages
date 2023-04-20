b642b6db63498f3d61178842bf01fa08a9f263d3
Ferisa Tri Putri Prestasi <50066797+prestasicode@users.noreply.github.com>
2023-04-20 23:20:52 +0700

    Update pandas.py

--- pandas.py
+++ pandas.py
@@ -1,1 +1,12 @@
+import pandas as pd
 
+#create init dataframe df1 and df2
+#create a view df2 from df1
+df1 = pd.DataFrame({"c1":[1,2], "c2":[3,4]})
+df2 = df1["c1"] 
+
+#change the value of df2 using iloc
+df2.iloc[0] = 7
+
+#check final value from df1
+df1 
