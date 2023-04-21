925c54d093d3f6327669e68c11c65e56fce1733a
Ferisa Tri Putri Prestasi <50066797+prestasicode@users.noreply.github.com>
2023-04-21 23:57:11 +0700

    Update pandas.py

--- pandas.py
+++ pandas.py
@@ -14,3 +14,12 @@ df1
 
 #pandas 2.0
 pandas.option.mode.copy_on_write = True
+
+df1 = pd.DataFrame({"c1":[1,2], "c2":[3,4]})
+df2 = df1["c1"] 
+
+#change the value of df2 using iloc
+df2.iloc[0] = 7
+
+#check final value from df1
+df1 #original dataframe isn't changed
