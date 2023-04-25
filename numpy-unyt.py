72b8711a9bd842aa35903a20397188167c13845b
Ferisa Tri Putri Prestasi <50066797+prestasicode@users.noreply.github.com>
2023-04-25 23:13:03 +0700

    Update numpy-unyt.py

--- numpy-unyt.py
+++ numpy-unyt.py
@@ -5,3 +5,15 @@ temps_f = (temps * 9/5) + 32
 print(temps_f)
 
 #[77. 86. 95. 104.]
+
+
+## 
+from unyt import degC, degF
+#array temp in Celcius degree
+temps = np.array([25, 30. 35, 40]) * degC
+#Convert to Fahrenheit degree
+temps_f = temps.to(degF)
+
+print(temps_f)
+
+##[77. 86. 95. 104.] Fahrenheit degree
